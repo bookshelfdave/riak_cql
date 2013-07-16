37f4b31981153f94ec9e53e316e4270168e2fce9
Dave Parfitt <dparfitt@basho.com>
2013-07-16 14:00:59 -0400

    simple test

--- test/riak_cql_tests.erl
+++ test/riak_cql_tests.erl
@@ -2,8 +2,13 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-simple_test() -> {ok, T} = riak_cql_parser:string("{counter foo++, counter bar--}"),
+simple1_test() -> {ok, T} = riak_cql_parser:string("{counter foo++}"),
+                 Expected = [{update,
+                              [{update,{"foo",riak_dt_pncounter},{increment,1}}]}],
+                 ?assertEqual(Expected, T).
+simple2_test() -> {ok, T} = riak_cql_parser:string("{counter foo++, counter bar--}"),
                  Expected = [{update,
                               [{update,{"foo",riak_dt_pncounter},{increment,1}},
                                {update,{"bar",riak_dt_pncounter},{decrement,1}}]}],
                  ?assertEqual(Expected, T).
+
